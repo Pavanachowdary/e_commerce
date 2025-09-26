315c3f0ebca4b7760d02c077344c9c6b89b98d31
Pavanachowdary <pavananettem21@gmail.com>
2025-09-26 21:09:05 +0530

    Add Sidebar.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./components/auth/LoginPage";
-import SignupPage from "./components/auth/SignupPage.jsx";
+import SignupPage from "./components/auth/signuppage";
 import AdminLayout from "./components/admin/AdminLayout";
 import NotFoundPage from "./pages/NotFoundPage";
 
