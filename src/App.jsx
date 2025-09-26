0750e437f4bf1dd12e710857846bbc2f58e75a96
Pavanachowdary <pavananettem21@gmail.com>
2025-09-26 20:39:58 +0530

    Fix SignupPage import case issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./components/auth/LoginPage";
-import SignupPage from "./components/auth/SignupPage";
+import SignupPage from "./components/auth/SignupPage.jsx";
 import AdminLayout from "./components/admin/AdminLayout";
 import NotFoundPage from "./pages/NotFoundPage";
 
